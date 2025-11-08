5c8e8e5eb0e74b48898c467833eef04a942059a5
Huy Phan <huyphanlop67@gmail.com>
2025-11-08 19:53:46 +0700

    OK!

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-// ⚠️ Dòng này đã được sửa lỗi cú pháp
+// ⚠️ Tên đúng phải là @vitejs/plugin-react (dùng gạch chéo '/')
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Đảm bảo bạn đã chạy 'npm install @vitejs/plugin-react'
-  plugins: [react()], 
+  plugins: [react()],
 })
